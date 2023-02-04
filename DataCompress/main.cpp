92d06f470a0c588710ba98a88f9bf2bcc60d8b41
layyue <layyue@tencent.com>
2023-02-04 17:04:44 +0800

    Initial Commit

---
+++ DataCompress/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataCompress
+//
+//  Created by Lei Yue on 2023/2/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
